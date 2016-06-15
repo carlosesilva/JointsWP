81ffeb05003c9672255d0610f853541c17f7d374
Carlos E Silva <csilva@mirandacreative.com>
2016-06-15 15:59:12 -0400

    add main.js, a file where you should put most of your javascript so that gulp can manipulate it

---
+++ assets/js/scripts/main.js
@@ -0,0 +1,11 @@
+/************************************************
+
+Script: Main Script
+put all of your javascript code in this file
+or create new files in this same directory
+The gulp task site-js will combine all the files in this directory into one
+
+*************************************************/
+jQuery(document).ready(function($) {
+    // your code here
+});
